feaaea9faec17190dba2b8cc39dba93375828662
unknown <brendomendonca20@gmail.com>
2023-12-18 20:46:11 -0300

    envio de códigos

--- 1185acima_diagonal_secundaria.py
+++ 1185acima_diagonal_secundaria.py
@@ -4,4 +4,12 @@ soma = quant = 0
 
 for i in range(3):
     for j in range(3):
-        
+        matriz[i].append(float(input()))
+        if(j <= (1 - i)):
+            soma += matriz[i][j]
+            quant += 1
+
+if(o == 'S'):
+    print(f"{soma:.1f}")
+else:
+    print(f"{soma/quant:.1f}")
