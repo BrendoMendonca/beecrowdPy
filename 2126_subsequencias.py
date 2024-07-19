f8ed05a2624dcced363ab54d31241ca7d5502893
unknown <brendomendonca20@gmail.com>
2024-07-18 21:07:57 -0300

    enviando código 2126 correto

--- 2126_subsequencias.py
+++ 2126_subsequencias.py
@@ -1,28 +1,25 @@
-caso = 0
-while(True):
+caso = 0 #inicialmente a quantidade de casos é zero
+while(True):#estrutura de repetição para recebimento dos casos
     try:
-        n1 = int(input())
-        n2 = int(input())
-        n1 = str(n1)
-        n2 = str(n2)
-        tam_n1 = len(n1)
-        caso += 1
-        qnt_sub = 0
+        #recebimento dos valores
+        n1 = input()
+        n2 = input()
+        tam_n1 = len(n1)#armazena o tamanho de n1
+        caso += 1 #incremento do valor da quantidade de casos
+        qnt_sub = 0#
         for i in range(0, len(n2)):
             if(n2[i] == n1[0]):
-                sequencia = []
-                if(sequencia[i:i+tam_n1] == n1):
+                if n2[i:i+tam_n1] == n1:
                     qnt_sub += 1
                     posicao = i+1
-        
-        if(qnt_sub !=0):
-            print(f"Caso: #{caso}")
-            print(f"Qtd. Subsequencias: {qnt_sub}")
-            print(f"Pos: {posicao}")
-        else:
-            print(f"Caso: #{caso}")
-            print("Nao existe subsequencia")
-        print(sequencia)
                     
+        print(f"Caso #{caso}:")
+        
+        if(qnt_sub != 0):
+            print(f"Qtd.Subsequencias: {qnt_sub}")
+            print(f"Pos: {posicao}\n")
+        else:
+            print("Nao existe subsequencia\n")
+                   
     except EOFError:
         break
