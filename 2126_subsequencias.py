05ff3ec68b150b7e9f24992d6bdf1d96be4ed179
unknown <brendomendonca20@gmail.com>
2024-07-19 13:35:57 -0300

    enviando código 2126 comentado

--- 2126_subsequencias.py
+++ 2126_subsequencias.py
@@ -6,13 +6,14 @@ while(True):#estrutura de repetição para recebimento dos casos
         n2 = input()
         tam_n1 = len(n1)#armazena o tamanho de n1
         caso += 1 #incremento do valor da quantidade de casos
-        qnt_sub = 0#
-        for i in range(0, len(n2)):
-            if(n2[i] == n1[0]):
-                if n2[i:i+tam_n1] == n1:
-                    qnt_sub += 1
-                    posicao = i+1
+        qnt_sub = 0#variável que armazena a quantidade de cada subsequencia
+        for i in range(0, len(n2)):#estrutura de repetição para verificar se há subsequencia
+            if(n2[i] == n1[0]):#verifica se os algarismos  da sequencia(n2) são iguais primeiro algarismo do número(n1)
+                if n2[i:i+tam_n1] == n1:#verifica se a sequência(n2) de i até i + tamanho de n1 é igual a n1
+                    qnt_sub += 1#incremento na quantidade de subsequencia
+                    posicao = i+1#incremento na posição da sequencia
                     
+        #impressões dos resultados
         print(f"Caso #{caso}:")
         
         if(qnt_sub != 0):
