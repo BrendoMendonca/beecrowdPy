831a15ddc727a2f6c07b6630bf613f59073efcf3
unknown <brendomendonca20@gmail.com>
2024-09-10 21:35:57 -0300

    atualização

--- 2502_decifrando_carta_criptografada.py
+++ 2502_decifrando_carta_criptografada.py
@@ -5,10 +5,10 @@ cifra_1 = input()
 cifra_2 = input()
 
 for _ in range(n):
-    sentenca = input().upper
+    sentenca = input().upper()
     
-    for indice, caractere in enumerate(sentenca):
-        for c in cifra_1:
+    for caractere in sentenca:
+        for indice, c in enumerate(cifra_1):
             if caractere == c:
                 caractere = cifra_2[indice]
 
