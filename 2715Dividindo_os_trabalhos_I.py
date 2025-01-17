c99999bce16a9c6048a63740a4c7056bf7ed6148
unknown <brendomendonca20@gmail.com>
2025-01-17 19:28:23 -0300

    enviando código 2715

--- 2715Dividindo_os_trabalhos_I.py
+++ 2715Dividindo_os_trabalhos_I.py
@@ -3,7 +3,7 @@ def calcular_diferenca_otima(n, trabalhos):
     soma_rangel = 0
     menor_diferenca = float('inf')
 
-    for i in range(n - 1):  #rangel não pode fazer todos os trabalhos
+    for i in range(n - 1): #rangel não pode fazer todos os trabalhos
         soma_rangel += trabalhos[i]
         soma_gugu = soma_total - soma_rangel
         diferenca = abs(soma_rangel - soma_gugu)
